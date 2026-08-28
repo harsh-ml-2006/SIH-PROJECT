572a0d36e0118aa103b65881eed0eb767ad47d0b
rahul-kumar011 <kumar264805@gmail.com>
2026-08-28 12:35:48 +0530

    Create locations.js

---
+++ GIS/data/locations.js
@@ -0,0 +1,35 @@
+const GIS_LOCATIONS = {
+
+    hospital: {
+        name: "KIMS Hospital",
+        type: "Hospital",
+        lat: 20.3568,
+        lng: 85.8150,
+        description: "Emergency medical support location"
+    },
+
+    shelter: {
+        name: "Emergency Shelter",
+        type: "Shelter",
+        lat: 20.3350,
+        lng: 85.8250,
+        description: "Emergency evacuation shelter"
+    },
+
+    osdma: {
+        name: "OSDMA",
+        type: "Disaster Management",
+        lat: 20.2726,
+        lng: 85.8390,
+        description: "Disaster management support location"
+    },
+
+    river: {
+        name: "Kuakhai River",
+        type: "River",
+        lat: 20.2870,
+        lng: 85.8750,
+        description: "River / flood-prone area"
+    }
+
+};
